c47e7db872d86503495ab0943219473b87d90a29
Jakub Wojcik <j.wojcik1994@gmail.com>
2021-10-19 21:43:21 +0200

    Project init, add styles

---
+++ main.js
@@ -0,0 +1,3 @@
+const spnText = document.querySelector('.text');
+const txt =
+  'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore tenetur rerum ad? Eum quos reprehenderit minima! Fugit, omnis! Eveniet repellendus et iure deserunt iusto quas in est odit, labore optio?';
