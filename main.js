00a1af77fdac7eab716c1c979ac680abd37e3ace
Jakub Wojcik <j.wojcik1994@gmail.com>
2021-10-22 13:23:30 +0200

    Working typing

--- main.js
+++ main.js
@@ -1,20 +1,42 @@
 const spnText = document.querySelector('.text');
 const spnCursor = document.querySelector('.cursor');
-const txt =
-  'Sometimes I’ll start a sentence and I don’t even know where it’s going. I just hope I find it along the way.';
+const sentences = [
+  'Sometimes I’ll start a sentence and I don’t even know where it’s going. I just hope I find it along the way.',
+  'I am running away from my responsibilities. And it feels good.',
+  'I just want to lie on the beach and eat hot dogs. That’s all I’ve ever wanted.',
+];
 
-let indexText = 0;
-const timeSetup = 100;
+let indexText = -25;
+let indexSentences = 0;
+const timeSetup = 400;
 
 const addLetter = () => {
-  spnText.textContent += txt[indexText];
+  if (indexText >= 0) {
+    spnText.textContent += sentences[indexSentences][indexText];
+  }
   indexText++;
-  if (indexText == txt.length) clearInterval(indexTyping);
+  if (indexText == sentences[indexSentences].length) {
+    indexSentences++;
+    if (indexSentences === sentences.length) {
+      return;
+    }
+
+    return setTimeout(() => {
+      indexText = -25;
+
+      spnText.textContent = '';
+      addLetter();
+    }, 3000);
+  }
+  setTimeout(addLetter, 40);
 };
 
+addLetter();
+
+// cursor
 spnCursor.textContent = '|';
 const cursorAnimation = () => {
   spnCursor.classList.toggle('active');
 };
-const indexTyping = setInterval(addLetter, timeSetup);
-setInterval(cursorAnimation, timeSetup * 4);
+
+setInterval(cursorAnimation, timeSetup);
